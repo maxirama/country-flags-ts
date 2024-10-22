bb0213634ef542dfdd24875d11cc404f7101b5d9
maxirama <maximilianogastonama@gmail.com>
2024-10-22 14:31:36 -0300

    feat: country card first implementation.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import { Header } from "../components/Header";
 import { SearchBar } from "../components/SearchBar";
 import { FiltersContainer } from "../components/FiltersContainer";
 import { Dropdown } from "../components/Dropdown";
+import data from "../assets/data.json";
+import { CountryCard } from "../components/CountryCard";
 
 export const Home = () => {
   return (
@@ -11,6 +13,17 @@ export const Home = () => {
         <SearchBar />
         <Dropdown />
       </FiltersContainer>
+      {data.map((country) => {
+        return (
+          <CountryCard
+            country={country.name}
+            flagSource={country.flags.png}
+            population={country.population}
+            capital={country.capital || "no tiene"}
+            region={country.region}
+          />
+        );
+      })}
     </>
   );
 };
