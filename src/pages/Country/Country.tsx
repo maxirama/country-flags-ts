14f0e8d2510e16163ea889f76e52fdacd39b6f8b
maxirama <maximilianogastonama@gmail.com>
2024-10-23 14:21:38 -0300

    feat: current country page filtering.

--- src/pages/Country/Country.tsx
+++ src/pages/Country/Country.tsx
@@ -3,9 +3,13 @@ import { Header } from "../../components/Header";
 import { IconButton } from "../../components/IconButton";
 import { CountryButton } from "../../components/CountryButton";
 import { useNavigate } from "react-router-dom";
+import data from "../../assets/data.json";
 export const CountryPage = () => {
   const { country } = useParams();
   const navigate = useNavigate();
+  const countryData = data.find(
+    (_country) => _country.name.toLowerCase() === country?.toLowerCase()
+  );
 
   const handleRedirect = () => {
     navigate("/");
