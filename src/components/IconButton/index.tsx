2d3abab6a4a4e4f234bff8eb2f07d30f6cb1e796
maxirama <maximilianogastonama@gmail.com>
2024-10-23 21:47:27 -0300

    feat: add country info

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -2,6 +2,7 @@ interface IconButtonProps {
   description: string;
   icon: string;
   handleClick: any;
+  border?: boolean;
 }
 
 import styles from "./styles/index.module.css";
@@ -10,9 +11,15 @@ export const IconButton: React.FC<IconButtonProps> = ({
   description,
   icon,
   handleClick,
+  border,
 }) => {
+  const borderProps = border ? "borded-button" : "";
+
   return (
-    <div className={styles["theme-container"]} onClick={handleClick}>
+    <div
+      className={`${styles["theme-container"]} ${styles[borderProps]}`}
+      onClick={handleClick}
+    >
       <i className={icon}></i>
       <p>{description}</p>
     </div>
